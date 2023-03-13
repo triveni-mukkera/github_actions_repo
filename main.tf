3cb15a9e410a93a7f77ce3530fce5c6b8017185f
triveni-mukkera <94029415+triveni-mukkera@users.noreply.github.com>
2023-03-13 13:10:23 +0000

    2nd commit

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 # provision to eu-west-2 region
 provider "aws" {
-  region  = "eu-west-2"
+  region  = var.region
 }
 resource "aws_instance" "app_server" {
-  ami           = "var.ami"
-  instance_type = "var.int_type"
+  ami           = var.ami
+  instance_type = var.int_type
  
 tags = {
     Name = var.ec2_name
